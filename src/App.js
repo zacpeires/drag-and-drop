c9247a2c5d9516e34c8304f4c7e48bd53f956c62
Zac Peires <zacpeires@googlemail.com>
2019-11-01 13:58:00 +0000

    feature: code for dragging and resizing - needs work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Draggable from './containers/Draggable';
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+      <Draggable />
+    </>
+  );
+}
+
+export default App;
