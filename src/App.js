d89874b6e92d7ad05faaa9cca44fc686b939bc8e
Zac Peires <zacpeires@googlemail.com>
2019-11-05 17:12:06 +0000

    feature: button toggles on hover

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Main from './containers/Main'
-import FileUpload from './components/FileUpload'
+import FileUpload from './components/FileUpload';
+import styled from 'styled-components';
 import './index.css'
 
 
 const App = () => {
+
+
   return (
     <>
       <Main />
-      <FileUpload />
-    </>
+      <FileUpload/>
+      </>
   );
 }
 
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,69 +1,88 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
+import defaultBackground from '../assets/images/default.jpg';
 
 const BackgroundImage = styled.img`
-${props => {
-    if (props.loaded) {
-        return `
-        width: 100%;
-        height: 100vh
-        object-fit: cover;
-        `
-    } else {
-        return `
-            display: none;
-        `
-    }
-}}
-`
+    position: absolute;
+    width: 100%;
+    height: 100vh
+    object-fit: cover;
+    top: 0;
+`;
 
 const FileInput = styled.input`
-    display: none;
-`
+  display: none;
+`;
 
 const StyledLabel = styled.label`
-    cursor: pointer;
-    background-color:  #e65252;
-    color: white;
-    font-family: arial;
-    width: max-content;
-    padding: 15px 20px;
-    font-weight: 500;
-    display: flex;
-    position: absolute;
-    top: 25px;
-    left: 25px;
-    z-index: 5;
-    ${props => {
-        if (props.loaded) {
-            return `
-                display: none;
-            `
-        }
-    }}
-`
+  cursor: pointer;
+  background-color: #e65252;
+  color: white;
+  font-family: arial;
+  width: max-content;
+  padding: 15px 20px;
+  font-weight: 500;
+  display: flex;
+  z-index: 5;
+  display: none;
+  ${props => {
+    if (props.showUploadButton) {
+      return `
+                display: flex;
+            `;
+    }
+  }}
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  top: 40px;
+  left: 40px;
+  height: 80px;
+  width: 150px;
+`;
 
 export default () => {
-    const imageRef = useRef(null);
-    const fileInputRef = useRef(null);
-    const [loaded, setLoaded] = useState(false);
+  const imageRef = useRef(null);
+  const fileInputRef = useRef(null);
+  const [loaded, setLoaded] = useState(false);
+  const [showUploadButton, setShowUploadButton] = useState(false);
 
-    const handleChange = (event) => {
-        const fileReference = event.target.files[0]
-        const reader = new FileReader();
-        console.log(reader)
-        reader.onload = event => {
-        imageRef.current.src = event.target.result
-        }
-        reader.readAsDataURL(fileReference);
-        setLoaded(true)
-    }
+  const handleChange = event => {
+    const fileReference = event.target.files[0];
+    const reader = new FileReader();
+    console.log(reader);
+    reader.onload = event => {
+      imageRef.current.src = event.target.result;
+    };
+    reader.readAsDataURL(fileReference);
+    setLoaded(true);
+  };
 
-    return (
-        <>
-        <FileInput type="file" id="file" ref={fileInputRef} name="file" accept="image/png, image/jpeg" onChange={handleChange} />
-        <BackgroundImage ref={imageRef} loaded={loaded}/>
-        <StyledLabel for="file" loaded={loaded}>Choose a file...</StyledLabel>
-        </>
-    )
-}
+  return (
+    <>
+      <FileInput
+        type='file'
+        id='file'
+        ref={fileInputRef}
+        name='file'
+        accept='image/png, image/jpeg'
+        onChange={handleChange}
+      />
+      <BackgroundImage
+        ref={imageRef}
+        loaded={loaded}
+        onMouseOver={() => setShowUploadButton(false)}
+      />
+      <ButtonContainer onMouseOver={() => setShowUploadButton(true)}>
+        <StyledLabel
+          for='file'
+          loaded={loaded}
+          showUploadButton={showUploadButton}
+        >
+          Choose a file...
+        </StyledLabel>
+      </ButtonContainer>
+    </>
+  );
+};
